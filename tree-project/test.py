c5b6c8e691c72626d34b8bc218b1a8584d817300
Shaila Fye <slf2167@columbia.edu>
2021-09-20 11:50:19 -0400

    start for newick parser new file

---
+++ tree-project/test.py
@@ -0,0 +1,8 @@
+newick = {'label': 'Node1', 'children': [{'label': 'Lilium', 'children': [], 'features': 0.1005098375}, {'label': 'Node2', 'children': [{'label': 'Zea', 'children': [], 'features': 0.0405061803}, {'label': 'Node3', 'children': [{'label': 'Triticum', 'children': [], 'features': 0.0022100542}, {'label': 'Hordeum', 'children': [], 'features': 0.0077439602}], 'features': 0.0226705269}], 'features': 0.0106110864}, {'label': 'Oryza', 'children': [], 'features': 0.0122492691}], 'features': None}
+
+print(newick)
+
+print(type(newick))
+
+print(newick['children'])
+
