f823ce4fc887a01bdd4d3cf8c674d9d944ef4cdd
Cindy Ruan <cxr2000@barnard.edu>
2021-10-04 11:35:15 -0400

    updated files

--- tree-project/seq_dict.py
+++ tree-project/seq_dict.py
@@ -5,13 +5,12 @@ def open_seq(file_name):
     infile.close()
 
     for line in range(0, len(lines), 2):
-        gene_name = lines[line].strip()[1:]
+        gene_name = lines[line].strip()[1:].split()[0]
         gene_sequences[gene_name] = lines[line+1].strip()
 
     return gene_sequences
 
 ancestral = open_seq('./ancestralseqs.fasta.nodes.txt')
 rbcl = open_seq('./grass_rbcl.txt')
-
 ancestral.update(rbcl)
-print(ancestral.keys())
+
--- tree-project/tree_functions.py
+++ tree-project/tree_functions.py
@@ -1,23 +1,29 @@
-# import numpy as np
+import numpy as np
+from seq_dict import *
 
-# def check_seq(seq1, seq2):
-#     # find shorter and do that range
-#     change_dict = {'AA': 0, 'CG': 0, 'GC': 0}
-#     for i in range(len(seq1)):
-#         if seq1[i] == seq2[i]:
-#             continue
-#         elif seq1[i] != seq2[i]:
-#             # want GT or GC or CG
-#             nuc_change = str(seq1[i]) + str(seq2[i])
-#             change_dict[nuc_change] += 1
-#     return change_dict
-#
-#
-# print(check_seq('ATCGATCGATCG', 'ATCGATGGATCC'))
+def check_seq(seq1, seq2):
+    # find shorter and do that range
+    change_dict = {'AA': 0, 'TT': 0, 'CC': 0, 'GG': 0, 'AT': 0, 'TA': 0, 'CG': 0, 'GC': 0,
+                   'TC': 0, 'TG': 0, 'CT': 0, 'GT': 0, 'AC': 0, 'AG': 0, 'CA': 0, 'GA': 0}
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            continue
+        elif seq1[i] != seq2[i]:
+            # want GT or GC or CG
+            nuc_change = str(seq1[i]) + str(seq2[i])
+            change_dict[nuc_change] += 1
+    return change_dict
 
 
-# def two_d_arr() -> np.array:
-#     arr_zero = np.full((5, 5), 0, dtype=int)
-#     return arr_zero
-#
-# print(two_d_arr())
+for k,v in ancestral.items():
+    print(k)
+    #seqs = list(ancestral.values())
+
+#print(check_seq(seqs[0], seqs[1]))
+
+
+'''def two_d_arr() -> np.array:
+     arr_zero = np.full((5, 5), 0, dtype=int)
+     return arr_zero
+
+print(two_d_arr())'''
